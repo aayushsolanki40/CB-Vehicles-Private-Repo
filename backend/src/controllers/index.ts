a29e05a9b34c6bd6329b6bd7151e00c882a600e4
Oleksandra Tiankina <ag7756862@gmail.com>
2024-02-18 23:36:32 +0200

    setup-deploy

--- backend/src/controllers/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as authControllers } from "./auth";
-
--- backend/src/helpers/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as HttpError } from "./HttpError";
-export { default as ctrlWrapper } from "./ctrlWrapper";
-export { default as handleMongooseError } from "./handleMongooseError";
-export { default as emailSender } from "./emailSender";
